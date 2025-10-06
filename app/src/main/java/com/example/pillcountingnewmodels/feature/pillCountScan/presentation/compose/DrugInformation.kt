e7b7ec4d1a8b2623420602310e5c5d8c456b047c
Pravin Kadam <pravin.kadam@ritetechnologies.co.in>
2025-10-06 12:07:27 +0530

    drud name text in one line

--- app/src/main/java/com/example/pillcountingnewmodels/feature/pillCountScan/presentation/compose/DrugInformation.kt
+++ app/src/main/java/com/example/pillcountingnewmodels/feature/pillCountScan/presentation/compose/DrugInformation.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pillcountingnewmodels.R
@@ -44,7 +45,9 @@ fun DrugInformation(uiState: PillScanningUiState) {
             fontSize = 14.sp,
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
-            color = AppTheme.extendedColors.textColor
+            color = AppTheme.extendedColors.textColor,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
 
         Spacer(modifier = Modifier.height(8.dp))
