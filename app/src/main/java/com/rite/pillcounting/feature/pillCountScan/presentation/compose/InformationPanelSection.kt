7b672a5cfd45b0ad932b743144f4daad88c2d613
Anurag <annurag2402@gmail.com>
2026-02-18 18:44:16 +0530

    UI changes (size)

--- app/src/main/java/com/rite/pillcounting/feature/pillCountScan/presentation/compose/InformationPanelSection.kt
+++ app/src/main/java/com/rite/pillcounting/feature/pillCountScan/presentation/compose/InformationPanelSection.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -24,7 +24,6 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -105,7 +104,9 @@ fun InformationPanelSection(
                     Icon(
                         painter = painterResource(id =R.drawable.delete),
                         contentDescription = "Reset",
-                        tint = MaterialTheme.colorScheme.primary
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(30.dp)
                     )
                 }
 
@@ -117,7 +118,9 @@ fun InformationPanelSection(
                             id = if (showHistory) R.drawable.scanning_foucs else R.drawable.history
                         ),
                         contentDescription = if (showHistory) "Focus" else "History",
-                        tint = MaterialTheme.colorScheme.primary
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(30.dp)
                     )
 
                 }
@@ -139,7 +142,10 @@ fun InformationPanelSection(
                     Icon(
                         painter = painterResource(id = R.drawable.delete),
                         contentDescription = "Reset",
-                        tint = MaterialTheme.colorScheme.primary
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(30.dp)
+
                     )
                 }
 
@@ -159,7 +165,9 @@ fun InformationPanelSection(
                             id = if (showHistory) R.drawable.scanning_foucs else R.drawable.history
                         ),
                         contentDescription = if (showHistory) "Focus" else "History",
-                        tint = MaterialTheme.colorScheme.primary
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(30.dp)
                     )
                 }
 
